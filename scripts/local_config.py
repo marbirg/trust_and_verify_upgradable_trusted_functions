2b75f9fa4dab42c1cc85e5613b932cf9a8f34992
Ubuntu <azureuser@sgx.1qriegsddzsubpmhndvc2eyiga.ax.internal.cloudapp.net>
2024-05-23 14:49:10 +0000

    use same template dir for local and enclave

--- scripts/local_config.py
+++ scripts/local_config.py
@@ -5,5 +5,5 @@ DAFNY_OUT = base+"/local/verified/"
 DAFNY_BIN = base+"/dafny/dafny"
 DAFNY_TARGET = '/compileTarget:py'
 INPUT_DIR=base+'/local/input/'
-TEMPLATE_DIR=base+'/local/templates/'
+TEMPLATE_DIR=base+'/templates/'
 DAFNY_PYTHON_SUFFIX = '-py'
